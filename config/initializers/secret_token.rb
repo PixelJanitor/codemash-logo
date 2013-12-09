760c337a726fd97d857418b90fd83c437b49c064
Derek Briggs <derek.briggs@me.com>
2013-12-09 10:37:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodemashLogo::Application.config.secret_key_base = 'e936819635ce64c17901dfea8abbc7f276d2e4750297dbac58565d6c4e8e0da2af4f5afeb3e9717cc1f1d70922fa549a3fe3386366b5852df84273f8ef78b781'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodemashLogo::Application.config.session_store :cookie_store, key: '_codemash-logo_session'
